4ab2f5146ebdbe449d4c983edb20868d0c5d9faf
Haklai-Class <Haklai-Class@outlook.com>
2026-05-11 14:58:22 +0300

    הוספת Admin נחמד וחמוד מאודמאוד

--- AprilRonenWebSite1/Login.aspx.cs
+++ AprilRonenWebSite1/Login.aspx.cs
@@ -20,6 +20,9 @@ public partial class Login : System.Web.UI.Page
                 Session["userName"] = Request.Form["userName"];
                 Session["isLoggedIn"] = true;
 
+                if (result == 2)
+                    Session["isAdmin"] = true;
+
                 Response.Redirect("Default.aspx");
             }
             else
@@ -32,6 +35,7 @@ public partial class Login : System.Web.UI.Page
     //returns:
     //0 - if user is not valid
     //1 - is user is valid
+    //2 - if user is admin
     private int GetUserTypeFromDB(string userName, string password)
     {
         string dbPath = this.MapPath("App_Data/Database.mdf");
@@ -45,6 +49,11 @@ public partial class Login : System.Web.UI.Page
 
         if (dt.Rows.Count == 1)
         {
+            DataRow row = dt.Rows[0];
+
+            if ((bool)row["is_admin"])
+                return 2;
+
             return 1;
         }
         else
